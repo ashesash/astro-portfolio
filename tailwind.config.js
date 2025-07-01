da37dcc7f46cdc70801186a004be9b314a90ae67
ashesash <ashsahu@outlook.com>
2025-07-02 02:49:53 +1000

    added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
@@ -8,10 +9,14 @@ export default {
         serif: ['Gabarito', 'ui-serif', 'serif'],
       },
       colors: {
-        primary: '#5755ff',
+        primary: 'oklch(58.8% 0.158 241.966)',
+        secondary:' oklch(25.7% 0.09 281.288)',
         neutral: '#94a3b8',
-        white: '#e2e8f0',
+        white: '#F2F5F6',
         black: '#0e141b',
+        primarylight: 'oklch(38.8% 0.158 241.966)',
+        secondarylight: 'oklch(55.7% 0.09 281.288)',
+        
       },
     },
   },
